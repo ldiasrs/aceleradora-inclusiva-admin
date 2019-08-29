6870a0eaa8eda3e5f7577aca8926f68cd8f15d22
Leonardo Dias <Leonardo_Dias@gap.com>
2019-08-29 16:30:08 -0300

    Adjust script

--- scripts/create-db.sh
+++ scripts/create-db.sh
@@ -2,5 +2,5 @@
 rm -rf app/db
 cd app
 mkdir db
-sqlite3 db/upload-hmw.db < ./app/config/sql-schema.sql
-sqlite3 db/upload-hmw.db < ./app/config/insert-students.sql
+sqlite3 db/upload-hmw.db < ./config/sql-schema.sql
+sqlite3 db/upload-hmw.db < ./config/insert-students.sql
