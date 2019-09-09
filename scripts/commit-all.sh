1824ff5c1e674af1aaaff6e9c76c6532b185cd1d
Leonardo Dias <Leonardo_Dias@gap.com>
2019-09-09 16:24:00 -0300

    Adjusting PWD command

--- scripts/commit-all.sh
+++ scripts/commit-all.sh
@@ -2,13 +2,13 @@ echo "Unziping files and removinf zip files"
 find ./data-works/ -name '*.zip' -exec sh -c 'unzip -d `dirname {}` {}' ';'
 find ./data-works/ -name "*.zip" -exec rm -rf {} \;
 echo "Looking dis folders"
-HOME="$(PWD)"
+HOME="$(pwd)"
 for line in $(find ./data-works -name dist); do 
   echo "Adjusting dist folder: $line"
   cd "$line"
   mv * ../../
   cd ../
-  RM_DIR="$(PWD)"
+  RM_DIR="$(pwd)"
   cd ..
   echo "Removing $RM_DIR"
   rm -rf "$RM_DIR"
