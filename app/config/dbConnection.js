6473d1a975538b0a148c375dd21711a2a235e75c
Leonardo Dias <Leonardo_Dias@gap.com>
2019-10-28 19:06:27 -0300

    Heruko support

--- app/config/dbConnection.js
+++ app/config/dbConnection.js
@@ -2,10 +2,10 @@ const sqlite3 = require('sqlite3').verbose();
 const fs = require('fs')
 const Promise = require('bluebird')
 
-const DB_PATH = 'app/db/upload-hmw.db'
+const DB_PATH = 'upload-hmw.db'
 //let db = new sqlite3.Database(':memory:');
 function createConnection() {
-    return new sqlite3.Database(DB_PATH, function (err) {
+    return new sqlite3.Database(DB_PATH,  sqlite3.OPEN_READWRITE | sqlite3.OPEN_CREATE,  function (err) {
         if (err) {
             console.log(`Error on connecting to DB: ${err}`)
             return
@@ -16,6 +16,7 @@ function createConnection() {
 }
 
 function migrate(dataFile) {
+    console.log(`Migranting SQL file: ${dataFile}`)
     fs.readFile(dataFile, 'utf8', function(err, dbSchema) {
         if (err) throw err;
         DB.exec(dbSchema, function(err){
