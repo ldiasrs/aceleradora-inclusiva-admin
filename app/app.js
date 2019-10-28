3d78abae5dcdaea6f47d33082a25b22c82eef0ce
Leonardo Dias <Leonardo_Dias@gap.com>
2019-10-28 19:10:45 -0300

    Heruko support

--- app/app.js
+++ app/app.js
@@ -1,5 +1,5 @@
 const app = require('./config/server')
 
-const port = 3000
+const port =  process.env.PORT || 8080;
 
 app.listen(port, () => console.log(`Delivery homework app listening on port ${port}!`))
